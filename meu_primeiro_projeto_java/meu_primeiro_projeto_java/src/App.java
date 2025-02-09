b83d35f7d46252081935992768b9467cfa4a0296
mateus-pereira1 <147281084+mateus-pereira1@users.noreply.github.com>
2025-02-09 15:01:59 -0300

    meu primeiro projeto java

---
+++ meu_primeiro_projeto_java/meu_primeiro_projeto_java/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá, mundo! Esse é meu primeiro passo nessa grande jornada!");
+    }
+}
